6181c053fc403d3e457801c2e7eaac7ae2ae5136
Ryan Brady <ryan.m.brady@stonybrook.edu>
2025-04-25 18:08:01 -0400

    seawulf notebook

---
+++ run_notebook.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+#SBATCH --job-name=job
+#SBATCH -p a100
+#SBATCH --gpus=1
+#SBATCH --time=03:00:00
+
+filter=F814W
+name="J1442+4055"
+cutout_data=/gpfs/scratch/usr/hst_doubles/cutout_data
+
+source activate env # if necessary, activate a conda env
+
+cd ${cutout_data}/${name}/${filter} 
+
+# define the input and output notebook paths
+input_notebook="model_${filter}.ipynb"
+output_notebook="model_${filter}.ipynb"
+
+# pass these notebook paths as arguments to the Python script
+papermill "$input_notebook" "$output_notebook"
