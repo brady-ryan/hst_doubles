262cd250b6764321fe6e832e420ae49e60339fa3
Ryan Brady <ryan.m.brady@stonybrook.edu>
2025-04-28 17:35:54 -0400

    use yaml and papermill

--- run_notebook.sh
+++ run_notebook.sh
@@ -7,15 +7,12 @@
 
 filter=F814W
 name="J1442+4055"
-cutout_data=/gpfs/scratch/usr/hst_doubles/cutout_data
 
 source activate env # if necessary, activate a conda env
 
-cd ${cutout_data}/${name}/${filter} 
-
 # define the input and output notebook paths
-input_notebook="model_${filter}.ipynb"
-output_notebook="model_${filter}.ipynb"
+input_notebook="uvis_model.ipynb"
+output_notebook="cutout_data/${name}/${filter}/model_${filter}.ipynb"
 
 # execute the notebook via papermill
-papermill "$input_notebook" "$output_notebook"
+papermill "$input_notebook" "$output_notebook" -f "yaml_files/${filter}/${name}.yaml"
